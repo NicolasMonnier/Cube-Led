a909f8ba845a9f0c1769a0314bbf9e14f1577065
cindraling <nicolas.monnier44@gmail.com>
2017-09-15 16:23:34 +0200

    modif

--- cube.cpp
+++ cube.cpp
@@ -2,8 +2,8 @@
 
 Cube::Cube(int size_x, int size_y, int size_z):_size_x(size_x), _size_y(size_y), _size_z(size_z)
 {
-  std::vector<std::vector<std::vector<int>>> vect(size_x,std::vector<std::vector<int>>(size_y,std::vector<int>(size_z,5)));
-  _vect = vect;
+  std::vector<std::vector<std::vector<int>>> vect(size_x,std::vector<std::vector<int>>(size_y,std::vector<int>(size_z,0)));
+  _cube = vect;
   vect.clear();
 }
 
@@ -11,8 +11,24 @@ Cube::~Cube()
 {
 }
 
+
 void Cube::print_My_Cube()
 {
-	std::cout<<_vect[0][0][0]<<std::endl;
+  std::cout<<_cube[0][0][0]<<std::endl;
 }
 
+void Cube::set_Voxel(int x, int y, int z, int state){
+  _cube[x][y][z] = state;
+}
+
+int Cube::get_voxel(int x, int y, int z){
+  return(_cube[x][y][z]);
+}
+
+void Cube::clear_Voxel(int x, int y, int z){
+  Cube::set_Voxel(x, y, z, 0);
+}
+
+void Cube::set_Cube(int state){
+
+}
--- include/cube.hpp
+++ include/cube.hpp
@@ -6,10 +6,18 @@ class Cube
 public: 
   Cube(int size_x, int size_y, int size_z);
   ~Cube();
+
   void print_My_Cube();
 
+  void set_Voxel(int x, int y, int z, int state);
+  int get_voxel(int x, int y, int z);
+  void clear_Voxel(int x, int y, int z);
+
+  void set_Cube(int state);
+  void clear_Cube();
+
 private:
-  std::vector<std::vector<std::vector<int>>> _vect;
+  std::vector<std::vector<std::vector<int>>> _cube;
 
   int _size_x;
   int _size_y;
